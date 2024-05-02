30b0b12c6eca7d30d290ad2f621bb56f112f0b60
Jared Irwin <jrairwin@sympatico.ca>
2024-05-02 11:20:44 -0400

    No cache on local runners

--- dist/index.js
+++ dist/index.js
@@ -26191,9 +26191,13 @@ async function cache(vcpkg_version) {
     if (vcpkg_version < new Date('2023-03-29')) {
         core.warning('The version of Vcpkg in use does not support Github Actions cache');
     }
+    else if (process.env.ACTIONS_CACHE_URL === undefined ||
+        process.env.ACTIONS_RUNTIME_TOKEN === undefined) {
+        core.warning('The runner in use does not support Github Actions cache');
+    }
     else {
-        core.exportVariable('ACTIONS_CACHE_URL', process.env.ACTIONS_CACHE_URL || '');
-        core.exportVariable('ACTIONS_RUNTIME_TOKEN', process.env.ACTIONS_RUNTIME_TOKEN || '');
+        core.exportVariable('ACTIONS_CACHE_URL', process.env.ACTIONS_CACHE_URL);
+        core.exportVariable('ACTIONS_RUNTIME_TOKEN', process.env.ACTIONS_RUNTIME_TOKEN);
         core.exportVariable('VCPKG_BINARY_SOURCES', 'clear;x-gha,readwrite');
     }
     core.endGroup();
--- src/cache.ts
+++ src/cache.ts
@@ -10,14 +10,16 @@ export async function cache(vcpkg_version: Date): Promise<void> {
     core.warning(
       'The version of Vcpkg in use does not support Github Actions cache'
     )
+  } else if (
+    process.env.ACTIONS_CACHE_URL === undefined ||
+    process.env.ACTIONS_RUNTIME_TOKEN === undefined
+  ) {
+    core.warning('The runner in use does not support Github Actions cache')
   } else {
-    core.exportVariable(
-      'ACTIONS_CACHE_URL',
-      process.env.ACTIONS_CACHE_URL || ''
-    )
+    core.exportVariable('ACTIONS_CACHE_URL', process.env.ACTIONS_CACHE_URL)
     core.exportVariable(
       'ACTIONS_RUNTIME_TOKEN',
-      process.env.ACTIONS_RUNTIME_TOKEN || ''
+      process.env.ACTIONS_RUNTIME_TOKEN
     )
     core.exportVariable('VCPKG_BINARY_SOURCES', 'clear;x-gha,readwrite')
   }
